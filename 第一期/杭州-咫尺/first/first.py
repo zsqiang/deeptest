af837493ceb497752c187cf06c2c642af700025a
Heatherwyz <1085413748@qq.com>
2018-01-11 10:31:01 +0800

    20180111 update

--- 第一期/杭州-咫尺/first/first.py
+++ 第一期/杭州-咫尺/first/first.py
@@ -1,4 +1,13 @@
 #-*- coding:utf-8 -*-
 __author__ = 'Heather'
-if __name__ == "__main__":
-    print("first")
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
