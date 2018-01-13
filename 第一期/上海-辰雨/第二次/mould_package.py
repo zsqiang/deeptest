43ed09b6e69e7e697c4d9ace6e36027e6b4173aa
chenyuLY187 <1178821890@qq.com>
2018-01-13 21:41:27 +0800

    Exercise Code

---
+++ 第一期/上海-辰雨/第二次/mould_package.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+模块导入的几种方式
+方式一：
+直接导入整个模块：
+import 模块名
+
+例如：
+import re
+
+方式二：
+从模块中导入指定的类，方法等资源：
+from 模块名 import 模块/类/方法/变量
+
+例如：
+import selenium
+from selenium import webdriver
+
+方法三：
+将模块中所有资源都导入：
+from 模块 import *
+
+例如：
+from sys import *
+'''
+
+
