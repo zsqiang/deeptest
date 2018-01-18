1ce9fb67eabb3a259d912d690f8fae27cde06399
susanhonly <susanhonly@163.com>
2018-01-18 17:31:58 +0800

    下班前提交

---
+++ 第一期/广州-33/快学python3示例练习代码/list_cut.py
@@ -0,0 +1,9 @@
+# coding = utf-8
+# 列表运用python的切片机制
+
+if __name__ == "__main__":
+    print("列表切片的示例:")
+    list1 = ["This", "is", "a", "sample", "for", "list"]
+
+    # 读取第二个元素is，注意索引下表从0开始
+    print("列表第2个元素是: %d" % list1.index(1))
