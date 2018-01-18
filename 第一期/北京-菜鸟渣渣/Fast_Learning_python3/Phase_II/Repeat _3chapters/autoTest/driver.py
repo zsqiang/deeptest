10571a298f2dd1c095e8e40b4e85ada6f7ccaf7c
yongweiye <vincentsys@yeah.net>
2018-01-18 18:51:58 +0800

    all changes file

---
+++ 第一期/北京-菜鸟渣渣/Fast_Learning_python3/Phase_II/Repeat _3chapters/autoTest/driver.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   Description :
+   Author :       Young
+   Change Activity: 2018/1/17:
+-------------------------------------------------
+"""
+__author__ = 'Young'
+
+class wbDriver:
+    def __init__(self):
+        pass
