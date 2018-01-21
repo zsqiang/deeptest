c7fea70bcef8ddb3b2ba0431c72e1bfdc34ed14a
lw2018 <lw20140705@163.com>
2018-01-22 00:06:42 +0800

    关于集合和函数

---
+++ 第一期/杭州-冬/第二次任务/function_eg.py
@@ -0,0 +1,16 @@
+#--coding:utf-8--
+#关于函数
+
+#求序列的和
+def summ(seq):
+    sum=0
+    for i in seq:
+        sum=sum+i
+    return sum
+
+def mult():
+    for i in range(1,j+1):
+        for x in range(1,j+1):
+            print("%s * %s=%d"%(i,j,i*j) )
+sum_1=summ([1,2,3,4,5])
+print(sum_1)
