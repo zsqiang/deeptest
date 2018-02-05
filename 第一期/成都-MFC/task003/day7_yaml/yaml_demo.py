9966eb59bd52316c88515aaa533d1a767df09765
MFC <moneyfromcat@foxmail.com>
2018-02-05 23:14:34 +0800

    add: day5 and day6 homework.
    add: day5 and day6 homework.

--- 第一期/成都-MFC/task003/day7_yaml/yaml_demo.py
+++ 第一期/成都-MFC/task003/day7_yaml/yaml_demo.py
@@ -5,5 +5,5 @@ __time__ = '18/1/31 22:08'
 
 """
 https://mp.weixin.qq.com/s?__biz=MzI0NDQ5NzYxNg==&mid=2247484127&idx=1&sn=fb89289f24417afc8318714080147102&scene=19#wechat_redirect
-研究robotframework的yaml语法结构，并形成学习记录
+作业：研究robotframework的yaml语法结构，并形成学习记录
 """
