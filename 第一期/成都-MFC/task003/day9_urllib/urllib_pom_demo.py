f44162b723d6298d1e2309e8f3f89c1816bd0b8d
MFC <moneyfromcat@foxmail.com>
2018-02-09 01:26:37 +0800

    add: finish day9 homework and extend demo.
    add: finish day9 homework and extend demo.

--- 第一期/成都-MFC/task003/day9_urllib/urllib_pom_demo.py
+++ 第一期/成都-MFC/task003/day9_urllib/urllib_pom_demo.py
@@ -4,6 +4,13 @@ __time__ = '18/2/7 22:35'
 
 
 """
+扩展:
+
+深入阅读urllib的源码和官方文档，熟悉其常用个API
+基于POM思想，封装urllib，形成基于urllib库的入门测试接口测试框架 -- 个人还是推荐用requests
+
+在httpclient_pom_demo.py的基础上修改
+
 测试对象为豆瓣图书相关开放的API：
 
 https://developers.douban.com/wiki/?title=book_v2
@@ -15,21 +22,22 @@ https://mp.weixin.qq.com/s?__biz=MzI0NDQ5NzYxNg==&mid=2247484146&idx=1&sn=8e4db3
 下面实例中所有代码的封装不采用python任何的高级特性，只使用基本特性
 
 必备知识：
-http.client常用API
+urllib常用API
 unittest
 logging
 PO模式
 
 命令行运行：
-python httpclient_pom_demo.py
+python urllib_pom_demo.py
 
-主要演示如何基于http.client + logging + unittest + pom进行基本的接口测试，
+主要演示如何基于urllib + logging + unittest + pom进行基本的接口测试，
 大家吸收下基本的思路就好，毕竟基于http.client这类的过于低层次的库来做还是太麻烦。
 """
 
 import logging
 import unittest
-import http.client
+import urllib.parse
+import urllib.request
 
 
 # 日志管理类
@@ -87,59 +95,49 @@ class CustomLogging:
         self.log.set_level(level)
 
 
-# http.client封装
+# urllib封装
 # http管理类
 class CustomHttp:
-    def __init__(self, protocol, host, port=80,
-                 key_file=None,  # ssl
-                 cert_file=None,  # ssl
-                 timeout=30,
-                 log_level=logging.INFO
-                 ):
+    def __init__(self, timeout=30, log_level=logging.INFO):
         self.log_level = log_level
         self.log = CustomLogging(level=log_level)
-        self.log.output("初始化http连接到: %s:%d" % (host, port))
+        self.log.output("日志初始化完成，日志级别： %s" % self.log_level)
 
-        self.host = host
-        self.port = port
         self.timeout = timeout
-        self.key_file = key_file
-        self.cert_file = cert_file
+
+        self.http = None
         self.response = None
         self.data = None
         self.status = None
         self.reason = None
         self.headers = None
 
-        self.http = None
-        if protocol == "http":
-            self.http = http.client.HTTPConnection(host=self.host,
-                                                   port=self.port, timeout=self.timeout)
-        elif protocol == "https":
-            self.http = http.client.HTTPSConnection(host=self.host,
-                                                    port=self.port,
-                                                    key_file=self.key_file,
-                                                    cert_file=self.cert_file,
-                                                    timeout=self.timeout)
-        else:
-            print("不支持的协议类型: ", protocol)
-            exit()
-
     # 返回response响应对象
     def request(self,
-                method,  # 请求方法 
+                method,  # 请求方法
                 url,  # 请求url
                 body=None,  # 请求数据
                 headers={}  # 请求头
                 ):
-        self.http.request(method=method, url=url, body=body, headers=headers)
 
-        self.response = self.http.getresponse()
+
+
+        response = urllib.request.urlopen(url)
+
+        if method == "GET":
+            response = urllib.request.urlopen(url)
+        elif method == "POST" and isinstance(body, dict):
+            data = bytes(urllib.parse.urlencode(body), encoding='utf8')
+            req = urllib.request.Request(url, data=data, headers=None)
+            response = urllib.request.urlopen(req)
+        else:
+            print("目前仅支持POST和GET方法的请求")
+
+        self.response = response
 
         self.data = self.response.read()
         self.status = self.response.status
         self.reason = self.response.reason
-        self.headers = self.response.getheaders()
         self.log.output("------" * 10, self.log_level)
         self.log.output("\nrequest")
         self.log.output("\nurl: %s \nmethod: %s \nheaders: %s \ndata: %s" %
@@ -150,22 +148,17 @@ class CustomHttp:
 
         return self.response
 
-    # 关闭连接
-    def close(self):
-        if self.http:
-            self.http.close()
-
     # 返回响应内容
     def get_data(self):
         return self.data
 
-    # 返回指定响应头
-    def get_header(self, name):
-        for header in self.headers:
-            if header[0] == name:
-                return header[1]
-
-        return None
+    # # 返回指定响应头
+    # def get_header(self, name):
+    #     for header in self.headers:
+    #         if header[0] == name:
+    #             return header[1]
+    #
+    #     return None
 
     # 返回完整的响应头
     def headers(self):
@@ -182,25 +175,11 @@ class Page:
         基类，所有的page models都需要继承该类
     """
 
-    def __init__(self, protocol, host, port=80,
-                 key_file=None,  # ssl
-                 cert_file=None,  # ssl
-                 timeout=30,
-                 log_level=logging.INFO):
-        self.http = CustomHttp(protocol=protocol,
-                               host=host,
-                               port=port,
-                               key_file=key_file,
-                               cert_file=cert_file,
-                               timeout=timeout,
-                               log_level=log_level)
+    def __init__(self, timeout=30, log_level=logging.INFO):
+        self.http = CustomHttp(timeout=timeout, log_level=log_level)
 
     def request(self, method, url, body=None, headers={}):
         self.http.request(method=method, url=url, body=None, headers={})
-
-    def close(self):
-        if self.http:
-            self.http.close()
 
 
 # 测试jsontest.com提供的API
@@ -210,22 +189,13 @@ class BookSearchPage(Page):
     """
     继承自基类
     """
-    def __init__(self, protocol, host, port=80,
-                 key_file=None,  # ssl
-                 cert_file=None,  # ssl
-                 timeout=30,
-                 log_level=logging.INFO):
-        Page.__init__(self, protocol=protocol,
-                      host=host,
-                      port=port,
-                      key_file=key_file,
-                      cert_file=cert_file,
-                      timeout=timeout,
+    def __init__(self, timeout=30, log_level=logging.INFO):
+        Page.__init__(self, timeout=timeout,
                       log_level=log_level)
 
     # 查询python相关的书籍
-    def search_python_book(self, method, url, body=None, headers={}):
-        self.request(method=method, url=url, body=body, headers=headers)
+    def search_python_book(self, method, url, uri, body=None, headers={}):
+        self.request(method=method, url=url+uri, body=body, headers=headers)
 
         return self.http.get_data()
 
@@ -233,11 +203,11 @@ class BookSearchPage(Page):
 # 测试用例
 class TestSearchBookPage(unittest.TestCase):
     def setUp(self):
-        self.book_search_page = BookSearchPage(protocol="https", host="api.douban.com", port=443)
+        self.book_search_page = BookSearchPage()
 
     def test_search_python_book(self):
         # 查找python相关的书籍即q=python，只找两本即count=2
-        books = self.book_search_page.search_python_book(method="GET", url="/v2/book/search?q=python&count=2")
+        books = self.book_search_page.search_python_book(method="GET", url="https://api.douban.com", uri="/v2/book/search?q=python&count=2")
 
         # 获取并断言下http status及reason
         status, reason = self.book_search_page.http.get_status_reason()
@@ -245,8 +215,9 @@ class TestSearchBookPage(unittest.TestCase):
         self.assertEqual(reason, "OK")
 
         # 获取并断言下http header 例如断言下返回的Content-Type是不是application/json; charset=utf-8
-        content_type = self.book_search_page.http.get_header("Content-Type")
-        self.assertEqual(content_type, "application/json; charset=utf-8")
+        # content_type = self.book_search_page.http.get_header("Content-Type")
+        # print("****" * 20)
+        # self.assertEqual(content_type, "application/json; charset=utf-8")
 
         # 看一下返回的数据类型
         print("/v2/book/search?q=python&count=2返回的数据类型为： ", type(books))
@@ -261,12 +232,12 @@ class TestSearchBookPage(unittest.TestCase):
         self.assertEqual(books_dict["count"], 2)
 
     def tearDown(self):
-        self.book_search_page.close()
+        print("Urllib POM Test End...")
 
 
 if __name__ == "__main__":
 
-    print("http.client Restful API测试实例")
+    print("Python3 urllib Restful API测试实例")
 
     unittest.main()
 
