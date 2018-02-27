56186dd5a8cf22e389f55506967cc5688007593b
nic <406722575@qq.com.com>
2018-02-27 19:34:21 +0800

    第一期001任务

--- 第一期/广州-番茄/task1/Calculation.py
+++ 第一期/广州-番茄/task1/Calculation.py
@@ -1,31 +1,35 @@
-#coding=utf-8
-
 class Calc:
     # 初始化
-    def __init__(self, a, b):
-        self.a=int(input("请输入一个整数："))
-        self.b=int(input("请输入一个整数："))
+    def __init__(self,a,b):
+        self.a =int(input("请输入第一个数字: "))
+        self.b =int(input("请输入第二个数字: "))
         if self.b==0:
-            print("不能为0")
-            self.b=input("请输入一个整数：")
+            print("第二个数字不能为0")
+            self.b =int(input("请输入第二个数字: "))
         else:
             self.b=b
-
     # 加法
     def __add__(self):
-        add=self.a+self.b
-        print(add)
-    
+        print("加法结果是:",end="")
+        return self.a+self.b
     # 减法
     def __sub__(self):
-        pass
-    
+        print("减法结果是:",end="")
+        return self.a-self.b
     # 乘法
-    def mul(self):
-        pass
-
+    def __mul__(self):
+        print("乘法结果是:",end="")
+        return self.a*self.b
     # 除法
-    def div(self):
-        pass
-Calc_obj=Calc(3,4)
-Calc_obj.__add__()
+    def __div__(self):
+        print("除法结果是:",end="")
+        return self.a/self.b
+     
+Calc_obj=Calc(6,8)
+print(Calc_obj.__add__())
+print(Calc_obj.__sub__())
+print(Calc_obj.__mul__())
+print(Calc_obj.__div__())
+
+
+
