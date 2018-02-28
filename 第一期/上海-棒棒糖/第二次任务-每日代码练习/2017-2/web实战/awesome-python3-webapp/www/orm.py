a58c3cf7062db1153e77cbbd3b5f839c05845443
879106261 <879106261@qq.com>
2018-02-28 21:33:04 +0800

    第二次任务-web搭建

--- 第一期/上海-棒棒糖/第二次任务-每日代码练习/2017-2/web实战/awesome-python3-webapp/www/orm.py
+++ 第一期/上海-棒棒糖/第二次任务-每日代码练习/2017-2/web实战/awesome-python3-webapp/www/orm.py
@@ -1,104 +1,134 @@
-import asyncio, logging
-import aiomysql
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+__author__ = 'Michael Liao'
+
+import asyncio, logging
+
+import aiomysql
+#args=()在调用log函数时，可传入参数如log(1,(5,6))
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
-
+#创建连接池
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
+    #定义全局变量，保存数据库连接信息
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', '121.199.13.222'),
+        host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
-        user=kw['root'],
-        password=kw['root'],
+        user=kw['user'],
+        password=kw['password'],
         db=kw['db'],
         charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
-        minsize=kw.get('minsize', 1),
         maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
         loop=loop
     )
-
+#select类
 async def select(sql, args, size=None):
+    #调用log函数
     log(sql, args)
-    global __pool
+    #global __pool
+    #使用get()方法连接数据库
     async with __pool.get() as conn:
+        # 获取游标,默认游标返回的结果为元组,每一项是另一个元组,通过aiomysql.DictCursor指定元组的元素为字典
         async with conn.cursor(aiomysql.DictCursor) as cur:
+            # 调用游标的execute()方法来执行sql语句,execute()接收两个参数,第一个为sql语句可以包含占位符,第二个为占位符对应的值,使用该形式可以避免直接使用字符串拼接出来的sql的注入攻击
+            #SQL语句的占位符是?，而MySQL的占位符是%s，自动替换
+            # args or ()表示参数可以是tuple类型
             await cur.execute(sql.replace('?', '%s'), args or ())
+            # size有值就获取对应数量的数据
             if size:
                 rs = await cur.fetchmany(size)
             else:
+                # 获取所有数据库中的所有数据,此处返回的是一个数组,数组元素为字典
                 rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
 
+#增删改类
 async def execute(sql, args, autocommit=True):
     log(sql)
     async with __pool.get() as conn:
+        #非自动提交事物，手动提交
         if not autocommit:
             await conn.begin()
         try:
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 await cur.execute(sql.replace('?', '%s'), args)
+                #cur.rowcount()获得影响的行数
                 affected = cur.rowcount
             if not autocommit:
+                # 提交事务
                 await conn.commit()
+        #如果报错，回滚
         except BaseException as e:
             if not autocommit:
+                #rollback() 回滚
                 await conn.rollback()
             raise
         return affected
 
+#创建拥有几个占位符的字符串
+#传入一个数字，在L中增加相应个数的？
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ', '.join(L)
 
+#ORM的思路就是把一个表映射成python中的类，行数据映射成类的实例
+#该类是为了保存数据库列名和类型的基类
 class Field(object):
-
     def __init__(self, name, column_type, primary_key, default):
-        self.name = name
-        self.column_type = column_type
-        self.primary_key = primary_key
-        self.default = default
+        self.name = name #列名
+        self.column_type = column_type #数据类型
+        self.primary_key = primary_key #是否为主键
+        self.default = default #默认值
 
+#self.__class__.__name__ 表示类名Field
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
+# 不同类型的字段类型，供不同类型的列调用，均继承自Field基类
+# 字段信息均有默认值，可缺省定义也可定制
+# 字符串类型
 class StringField(Field):
-
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        #super().__init__调用父类Field的__init__方法
         super().__init__(name, ddl, primary_key, default)
-
+# 布尔值类型
 class BooleanField(Field):
-
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
-
+# 整数类型
 class IntegerField(Field):
-
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
-
+# 浮点数类型
 class FloatField(Field):
-
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
-
+# 文本类型
 class TextField(Field):
-
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
+#元类
+#metaclass是类的模板，所以必须从`type`类型派生
 class ModelMetaclass(type):
-
+    #attrs传入一个未命名的变量参数，*attrs所有未命名的变量参数
+    #类方法的第一个参数是类对象cls，那么通过cls引用的必定是类对象的属性和方法
+    #不理解__new__()
     def __new__(cls, name, bases, attrs):
         if name=='Model':
             return type.__new__(cls, name, bases, attrs)
+        # 保存表名,如果获取不到,则把类名当做表名,完美利用了or短路原理
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
+        # 保存列类型的对象
         mappings = dict()
         fields = []
         primaryKey = None
@@ -129,7 +159,6 @@ class ModelMetaclass(type):
         return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass=ModelMetaclass):
-
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -202,6 +231,7 @@ class Model(dict, metaclass=ModelMetaclass):
             return None
         return cls(**rs[0])
 
+    # 保存实例到数据库
     async def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primary_key__))
@@ -209,6 +239,7 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to insert record: affected rows: %s' % rows)
 
+    # 更新数据库数据
     async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primary_key__))
@@ -216,8 +247,11 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to update by primary key: affected rows: %s' % rows)
 
+    # 删除数据
     async def remove(self):
         args = [self.getValue(self.__primary_key__)]
-        rows = await execute(self.__delete__, args)
+        rows =await execute(self.__delete__, args)
         if rows != 1:
             logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+
+
